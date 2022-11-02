1f8ba3f0871ec7047e5ccb2baeb56d6e66d0b68e
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-02 16:06:59 +0100

    Aggiunto ciclo for  e creato elementi div contenenti i numeri per la tabella

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//seleziono il contenitore dei numeri
+const boardContainer = document.querySelector(".board");
+
+//creo un ciclo con for
+for(let i = 0; i <= 100; i++){
+    //creo un elemento div
+    const boardCell = document.createElement("div");
+    //inserisco contenuto nel mio div
+    boardCell.innerHTML = i;
+    //assegno una classe al mio div
+    boardCell.classList.add("board_number");
+    //inserisco il tutto nel mio file html
+    boardContainer.append(boardCell);
+
+}
